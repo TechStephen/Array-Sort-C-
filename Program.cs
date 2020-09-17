48e2ffbdb179b6618fd94b0430c19195951cad9c
TechStephen <68354435+TechStephen@users.noreply.github.com>
2020-09-17 19:24:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 5, 0, 14, 400, 64 };
+            sortBigtoSmall(nums);
+            sortSmalltoBig(nums);
+        }
+
+        public static void sortBigtoSmall(int[] nums)
+        {
+            int[] temp = new int[nums.Length];
+            int counter = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if(nums[i] > nums[j])
+                    {
+                        counter++;
+                    }
+                }
+                temp[counter] = nums[i];
+                counter = 0;
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", temp));
+        }
+
+        public static void sortSmalltoBig(int[] nums)
+        {
+            int[] temp = new int[nums.Length];
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] < nums[j])
+                    {
+                        counter++;
+                    }
+                }
+                temp[counter] = nums[i];
+                counter = 0;
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", temp));
+        }
+    }
+}
